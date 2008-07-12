9bef0d79d85201c6bfe6c25ecd267e10de8e2787
Jeff Keen <jeff@jeffreykeen.com>
2008-07-12 11:19:08 -0500

    reorganized a few things

--- oneclickconversationsforgmail.user.js
+++ oneclickconversationsforgmail.user.js
@@ -1,37 +1,37 @@
 // ==UserScript==
-// @name          One Click Conversations for GMail
+// @name           One Click Conversations for GMail
 // @namespace      http://www.jeffreykeen.com/projects/oneclickconversations
-// @description   Access recent conversations in one painless click.
+// @description    Access recent conversations in one painless click.
 // @include        http://mail.gmail.com/*
 // @include        https://mail.gmail.com/*
 // @include        http://mail.google.com/*
 // @include        https://mail.google.com/*
 // ==/UserScript==
 
-/* 
+/*
+
 ### Author: Jeff Keen - http://www.jeffreykeen.com
 	
 ###	Features, as of latest release (For Gmail V2 users): ----------------------------------------------
 
-	 + Adds icon just to the left of sender name in list view and in message view
-	    + Clicking on icon takes you to the recent conversations with that user	
-		+ Option-clicking the icon searches for all messages from the sender's domain.
+  + Adds icon just to the left of sender name in list view and in message view
+    + Clicking on icon takes you to the recent conversations with that user	
+  + Option-clicking the icon searches for all messages from the sender's domain.
 
 ### Version History:
-       1.0   - 12.29.2006 - Initial Release
-       1.1.0 - 12.30.2006 - One bug fix turned into major restructuring.  
-       1.1.1 - 01.17.2007 - Checks if chat is enabled to prevent icons from showing up without any functionality.
-       1.1.2 - 04.06.2007 - I changed the name from Quicker Contacts to One Click Conversations, a name that better describes what the main use of this script is.  
-       1.1.3 - 04.19.2007 - The script now alerts you if chat isn't enabled, and provides a link to enable it.
-       1.2.0 - 06.20.2007 - Fixed logic after google changed their code.
-		                  - Added rollover image to icon as well as tooltip, to make it more obvious that it's clickable.
-	   1.2.1 - 07.03.2007 - Fixed slight bug which occurred when clicking "Expand All" in message view.  
-	                      - Changed XPath function to allow Opera Support.
-						  - Fixed bug (due to changed Google code) so clicking on the icon wouldn't pull up messages to and from yourself in the case of multiple replies.
-	   1.2.2 - 08.28.2007 - Added more event listeners to ensure that icons get reapplied after an event (deleting a message, for instance).
-	   2.0	 - 12.14.2007 - Rewrote script to add support for GMail V2, using Google's API. 
-	   2.1   - 7.4.2008   - Added better cleanup of old event listeners, and reduced use of event listeners dramatically, which improves memory usage
-
+   1.0   - 12.29.2006 - Initial Release
+   1.1.0 - 12.30.2006 - One bug fix turned into major restructuring.  
+   1.1.1 - 01.17.2007 - Checks if chat is enabled to prevent icons from showing up without any functionality.
+   1.1.2 - 04.06.2007 - I changed the name from Quicker Contacts to One Click Conversations, a name that better describes what the main use of this script is.  
+   1.1.3 - 04.19.2007 - The script now alerts you if chat isn't enabled, and provides a link to enable it.
+   1.2.0 - 06.20.2007 - Fixed logic after google changed their code.
+                      - Added rollover image to icon as well as tooltip, to make it more obvious that it's clickable.
+   1.2.1 - 07.03.2007 - Fixed slight bug which occurred when clicking "Expand All" in message view.  
+                      - Changed XPath function to allow Opera Support.
+  			              - Fixed bug (due to changed Google code) so clicking on the icon wouldn't pull up messages to and from yourself in the case of multiple replies.
+   1.2.2 - 08.28.2007 - Added more event listeners to ensure that icons get reapplied after an event (deleting a message, for instance).
+   2.0	 - 12.14.2007 - Rewrote script to add support for GMail V2, using Google's API. 
+   2.1   - 7.4.2008   - Added better cleanup of old event listeners, and reduced use of event listeners dramatically, which improves memory usage
 */
 const CLOCK_IMAGE = "data:image/gif;base64,R0lGODlhCgAKAKIAADMzM//M/7CwsGZmZv///8fHxwAAAAAAACH5BAEHAAEALAAAAAAKAAoAAAMpGDo8+kOUItwqJJPioh5ZNWAEmHHjdzKCRrRVAJAn8AASZT/BAACWQAIAOw==";
 const PERSON_IMAGE_OVER = "data:image/gif;base64,R0lGODlhCgAKALMAADMzM//M/9LS0mZmZrm5ue7u7v///+rq6t3d3QAAAAAAAAAAAAAAAAAAAAAA" +
@@ -64,6 +64,7 @@ const CONV_TO_SPAN_CLASS='HcCDpe';
 const CONV_ADDRESS_TABLE= 'BwDhwd';
 const CONV_IMG_ID = 'upi';
 
+/* Add Styles */
 var css = "span.oneclick{background: transparent url(" + PERSON_IMAGE + ")  left no-repeat; display:inline-block; text-align:right; padding-right:5px; width:10px; height:10px;} span.oneclick:hover{background: transparent url(" + PERSON_IMAGE_OVER + ")  left no-repeat;}";
 if (typeof GM_addStyle != "undefined") {
 	GM_addStyle(css);
@@ -95,8 +96,7 @@ window.addEventListener('load', function() {
 			  XPathResult.ORDERED_NODE_ITERATOR_TYPE,
 			  null); // no existing results
 		  } catch (err) {
-			GM_log("Error when evaluating XPath expression '" + expression + "'" +
-				   ": " + err);
+			GM_log("Error when evaluating XPath expression '" + expression + "'" + ": " + err);
 			return null;
 		  }
 		  var results = [];
@@ -108,21 +108,11 @@ window.addEventListener('load', function() {
 			
 		  return results;
 		}
-		
-		function addIcons() {
-			/* Calls appropriate functions for adding One Click icon, based on active view */
-			var $view=gmail.getActiveViewType();
-			if ($view == 'cv') { //conversation view
-				listen(false);
-				modConversationView();
-				listen(true);
-			}
-			else if ($view == 'tl') { //list view
-				listen(false);
-				modListView();
-				listen(true);
-			}
-			toggleListeners(gmail.getActiveViewElement());
+	
+		function listen(bool) {
+			var root=gmail.getActiveViewElement();
+			if (bool)	root.addEventListener("DOMNodeInserted", addIcons, false); 		
+			else root.removeEventListener("DOMNodeInserted", addIcons, false); 
 		}
 	
 	  function toggleListeners(page) {
@@ -132,17 +122,50 @@ window.addEventListener('load', function() {
 			turnOnListeners(iconListeners[view_id]);
 			removeOrphans();
 	  }
-	
-		function listen(bool) {
-			var root=gmail.getActiveViewElement();
-			if (bool==true) {		
-				root.addEventListener("DOMNodeInserted", addIcons, false); 		
-			}
-			if (bool==false) {
-				root.removeEventListener("DOMNodeInserted", addIcons, false); 
-			}
+		
+		function turnOffListeners(array) {
+		  for (var i=0; i < array.length; i++) {
+		    array[i].removeEventListener('mousedown', jumpToConversation, false);
+		  }
 		}
-	
+		
+		function turnOnListeners(array) {
+		  for (var i=0; i < array.length; i++) {
+		    array[i].addEventListener('mousedown', jumpToConversation, false);
+		  }
+		}
+		
+		function removeIconListener(element) {
+		  element.removeEventListener('mousedown', jumpToConversation, false);
+		}
+				
+		function removeOrphans() {
+		  for (var id in iconListeners) {
+		     if ((document.getElementById('canvas_frame')) && (!document.getElementById('canvas_frame').contentDocument.getElementById(id))) {
+		       turnOffListeners(iconListeners[page]);
+		       delete iconListeners[page];
+		     }
+		  }
+		}
+		
+		function trackIcon(page, element) {
+		  /* keep track of icons per page, so we can add/remove listeners effectively */
+		  var listeners = [];
+		  if (key = page.getAttribute('view_id')) {
+        listeners = iconListeners[key];
+		  }
+		  else {
+		    key = "vid_" + parseInt(Math.random() * 10000000, 10);
+		    page.setAttribute('view_id', key);
+		  }
+		  listeners.push(element);
+		  iconListeners[key] = listeners;
+		}
+		
+	  function isModified(message) {
+			return (message.innerHTML.match(/class=\"oneclick\"/));
+	  }
+			
 		function getMyEmailAddress() {
 			/* get our address, so we don't create link to ourself in the case of conversations */
 			try {
@@ -184,49 +207,23 @@ window.addEventListener('load', function() {
       clickSpan.textContent = " ";
       return clickSpan;
 		}
-		
-		function trackIcon(page, element) {
-		  var listeners = [];
-		  if (key = page.getAttribute('view_id')) {
-        listeners = iconListeners[key];
-		  }
-		  else {
-		    key = "vid_" + parseInt(Math.random() * 10000000, 10);
-		    page.setAttribute('view_id', key);
-		  }
-		  listeners.push(element);
-		  iconListeners[key] = listeners;
+			
+		function addIcons() {
+			/* Calls appropriate functions for adding One Click icon, based on active view */
+			var $view=gmail.getActiveViewType();
+			if ($view == 'cv') { //conversation view
+				listen(false);
+				modConversationView();
+				listen(true);
+			}
+			else if ($view == 'tl') { //list view
+				listen(false);
+				modListView();
+				listen(true);
+			}
+			toggleListeners(gmail.getActiveViewElement());
 		}
-		
-		function removeIconListener(element) {
-		  element.removeEventListener('mousedown', jumpToConversation, false);
-		}
-		
-		function turnOffListeners(array) {
-		  for (var i=0; i < array.length; i++) {
-		    array[i].removeEventListener('mousedown', jumpToConversation, false);
-		  }
-		}
-		
-		function turnOnListeners(array) {
-		  for (var i=0; i < array.length; i++) {
-		    array[i].addEventListener('mousedown', jumpToConversation, false);
-		  }
-		}
-		
-		function removeOrphans() {
-		  for (var id in iconListeners) {
-		     if ((document.getElementById('canvas_frame')) && (!document.getElementById('canvas_frame').contentDocument.getElementById(id))) {
-		       turnOffListeners(iconListeners[page]);
-		       delete iconListeners[page];
-		     }
-		  }
-		}
-		
-	  function isModified(message) {
-			return (message.innerHTML.match(/class=\"oneclick\"/));
-	  }
-	
+			
 		function modListView() {
 			var myEmailAddress=getMyEmailAddress();
 			var page = gmail.getActiveViewElement();
